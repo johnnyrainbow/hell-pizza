f8876708f2d63b919e1c27641ac2a8950db70af6
Gabriel Kennedy <gabriel.m.kennedy@gmail.com>
2019-02-14 18:44:00 +1300

    http-json refactor

--- src/http-json.js
+++ src/http-json.js
@@ -3,12 +3,11 @@ var urls = require('./urls.json')
 
 module.exports.post = function (url, req, callback) {
     if (typeof req != 'string')
-        req = JSON.stringify(req);
+        req = JSON.stringify(req)
 
     var requestBody = {
         uri: url,
         headers: {
-            Referer: urls.referer,
             'Content-Type': 'application/json'
         },
         body: req
@@ -16,54 +15,11 @@ module.exports.post = function (url, req, callback) {
 
     http.post(requestBody, function (error, res, body) {
         if (error) {
-            return callback({
-                success: false,
-                message: error
-            });
-        }
-
-        if (res.statusCode !== 200) {
-            return callback({
-                success: false,
-                message: 'HTML Status Code Error ' + res.statusCode
-            });
-        }
-
-        try {
-            var parsed = JSON.parse(body);
-        }
-        catch (error) {
-            console.log(error);
-            return callback({
-                success: false,
-                message: error
-            });
-        }
-
-        return callback({
-            success: true,
-            result: parsed
-        });
-    });
-}
-
-module.exports.get = function (url, callback) {
-    var requestBody = {
-        uri: url,
-        headers: {
-            'Referer': urls.referer
-        }
-    };
-
-    http.get(url, function (error, res, body) {
-        if (error) {  //If request errored out.
             return callback(error)
-
         }
-        if (res.statusCode !== 200) {  //If request didn't error but response isn't status code 200.
+        if (res.statusCode !== 200) {
             return callback('HTML Status Code Error ' + res.statusCode)
         }
-
         try {
             var parsed = JSON.parse(body);
         }
@@ -71,6 +27,24 @@ module.exports.get = function (url, callback) {
             return callback(error)
         }
         return callback(null, parsed)
-    }
-    );
+    });
+}
+
+module.exports.get = function (url, callback) {
+
+    http.get(url, function (error, res, body) {
+        if (error) {  //If request errored out.
+            return callback(error)
+        }
+        if (res.statusCode !== 200) {  //If request didn't error but response isn't status code 200.
+            return callback('HTML Status Code Error ' + res.statusCode)
+        }
+        try {
+            var parsed = JSON.parse(body);
+        }
+        catch (error) {
+            return callback(error)
+        }
+        return callback(null, parsed)
+    });
 }
